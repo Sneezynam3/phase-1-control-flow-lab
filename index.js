c0a2cb6824829bab62806d5420d0e86a2ac163f4
James Snell <vulpes3.1415@yahoo.com>
2023-11-29 16:49:09 -0700

    Control Flow, big time

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+    if (ride <= 400) {
+      return 'This one is on me!';
+    } else if (ride > 400 && ride < 2000) {
+      return 'That will be twenty bucks.';
+    } else if (ride >= 2000 && ride <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else if (ride > 2500) {
+      return 'No can do.';
+    }
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+// Write your code here!
+ return city ? 'Ok, sounds good.' : 'No go.' ;
 }
+//condition ? exprIfTrue : exprIfFalse
+//false is null, undefined, false, NaN, 0, -0, 0n, "", object
+//How to trick ternary into a false expression? Bang did not work 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+// Write your code here!
+  switch(generous) {
+    case 'generous':
+      return "Thank you so much.";
+      // break;
+    case 'not as generous':
+      return "Thank you.";
+      // break;
+    default:
+      return "Bye."
+  } 
 }
